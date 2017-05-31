a87395200f2290a739aae9b703c47b8b159ee596
Brado Subramanian <subramanian.bharadwaj@gmail.com>
2017-05-31 16:56:55 -0700

    Move ansible playbooks to separate folder

--- config-server.sh
+++ config-server.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ansible-playbook -i hosts get_ip.yml
-ansible-playbook -i hosts barman_setup.yml
-ansible-playbook -i hosts postgresql_install.yml
-ansible-playbook -i hosts db_setup.yml
-ansible-playbook -i hosts barman_after.yml
-ansible-playbook -i hosts standby_after.yml
-ansible-playbook -i hosts barman_standby.yml
+ansible-playbook -i hosts playbooks/get_ip.yml
+ansible-playbook -i hosts playbooks/barman_setup.yml
+ansible-playbook -i hosts playbooks/postgresql_install.yml
+ansible-playbook -i hosts playbooks/db_setup.yml
+ansible-playbook -i hosts playbooks/barman_after.yml
+ansible-playbook -i hosts playbooks/standby_after.yml
+ansible-playbook -i hosts playbooks/barman_standby.yml

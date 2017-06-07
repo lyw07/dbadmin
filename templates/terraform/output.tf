c25209a27482ab411f2f6c70587d0df6fc86712e
Brado Subramanian <subramanian.bharadwaj@gmail.com>
2017-06-06 18:06:48 -0700

    add back removed standby outputs

--- templates/terraform/output.tf
+++ templates/terraform/output.tf
@@ -6,6 +6,24 @@ output "barman_external_ip" {
     value = "${google_compute_instance.barman.*.network_interface.0.access_config.0.assigned_nat_ip}"
 }
 
+output "<[ master.hostname ]>_internal_ip" {
+    value = "${google_compute_instance.<[ master.hostname ]>.*.network_interface.0.address}"		
+}		
+		
+output "<[ master.hostname ]>_external_ip" {		
+    value = "${google_compute_instance.<[ master.hostname ]>.*.network_interface.0.access_config.0.assigned_nat_ip}"		
+}		
+		
+<[ #standby ]>
+output "<[ hostname ]>_internal_ip" {
+    value = "${google_compute_instance.<[ hostname ]>.*.network_interface.0.address}"
+}	
+        
+output "<[ hostname ]>_external_ip" {
+    value = "${google_compute_instance.<[ hostname ]>.*.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+<[ /standby ]>
+
 <[ #replicas ]>
 output "<[ hostname ]>_internal_ip" {
     value = "${google_compute_instance.<[ hostname ]>.*.network_interface.0.address}"

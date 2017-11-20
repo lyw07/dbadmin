a8e8fc5699dd36c2f243614e6e3d70b7a30285f9
Lingyi Wang <lywang07@gmail.com>
2017-11-20 14:44:26 -0800

    pause after cloning and change the credential name set in promote.sh

--- templates/scripts/promote.sh
+++ templates/scripts/promote.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "This is the promote script"
 repmgr standby promote -f /etc/repmgr.conf --log-to-file
-WALE_GS_PREFIX="gs://teststudiobackup/" GOOGLE_APPLICATION_CREDENTIALS=/etc/wal-e.d/env/GOOGLE_APPLICATION_CREDENTIALS wal-e backup-push /var/lib/postgresql/9.6/main/
+WALE_GS_PREFIX="gs://teststudiobackup/" GOOGLE_APPLICATION_CREDENTIALS=/etc/wal-e.d/env/google_application_credentials.json wal-e backup-push /var/lib/postgresql/9.6/main/

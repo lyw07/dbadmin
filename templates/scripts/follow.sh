9629739dc067703212196be46e08ac2e821e1561
Lingyi Wang <lywang07@gmail.com>
2017-11-20 22:13:42 -0800

    let it sleep

--- templates/scripts/follow.sh
+++ templates/scripts/follow.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "This is the follow script"
+sleep 30  # Let promote command finish so that follow command can find the new master node
 repmgr standby follow -f /etc/repmgr.conf --log-to-file

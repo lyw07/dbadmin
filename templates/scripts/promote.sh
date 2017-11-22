72e0e6602e7a39a5ab7288ac11c87ad4a6c72e77
Lingyi Wang <lywang07@gmail.com>
2017-11-22 14:17:41 -0800

    add trigger file

--- templates/scripts/promote.sh
+++ templates/scripts/promote.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "This is the promote script"
+touch /etc/wal-e.d/env/postgresql.trigger
 repmgr standby promote -f /etc/repmgr.conf --log-to-file
 # WALE_GS_PREFIX="gs://teststudiobackup/" GOOGLE_APPLICATION_CREDENTIALS=/etc/wal-e.d/env/google_application_credentials.json wal-e backup-push /var/lib/postgresql/9.6/main/

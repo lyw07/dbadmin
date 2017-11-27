f71dc772271ea4c3fa19758154a865be57569333
Lingyi Wang <lywang07@gmail.com>
2017-11-27 12:58:44 -0800

    testing

--- templates/scripts/promote.sh
+++ templates/scripts/promote.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "This is the promote script" >> /var/log/postgresql/postgresql-9.6-main.log
 touch /var/lib/postgresql/postgresql.trigger
-sleep 30
 repmgr standby promote -f /etc/repmgr.conf --log-to-file
 # WALE_GS_PREFIX="gs://teststudiobackup/" GOOGLE_APPLICATION_CREDENTIALS=/etc/wal-e.d/env/google_application_credentials.json wal-e backup-push /var/lib/postgresql/9.6/main/
